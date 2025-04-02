b7718b7f1ceaeeaef3a9f56b0fdf210b2d24e86a
Anshuman <anshuman.mishra1055@gmail.com>
2025-04-02 18:04:57 +0530

    filter and notification for subscribe done

--- api/Controller/subscribeController.js
+++ api/Controller/subscribeController.js
@@ -1,7 +1,9 @@
 import asyncHandler from "express-async-handler";
 import { makeDb } from "../db-config.js";
 import {
+    createNotification,
     createQueryBuilder,
+    getAdminData,
     storeError,
     updateQueryBuilder,
 } from "../helper/general.js";
@@ -12,6 +14,7 @@ const db = makeDb();
 export const createUpdateSubscribe = asyncHandler(async (req, res) => {
     try {
         const { id, email } = req.body;
+        const io = req.app.get("io");
         const checkEmailQuery = `SELECT id FROM subscribe WHERE email = ?`;
         const existingEmail = await db.query(checkEmailQuery, [email]);
 
@@ -34,7 +37,16 @@ export const createUpdateSubscribe = asyncHandler(async (req, res) => {
         } else {
             statusCode = 201;
             const { query, values } = createQueryBuilder(Subscribe, dataToSave);
-            await db.query(query, values);
+            const result = await db.query(query, values);
+            const admin = await getAdminData();
+            const data = {
+                user_id: admin?.id,
+                subject: "Subscribe",
+                message: `User ${email} has subscribed you`,
+                redirect_url: "/others/newsletter",
+                view_notify: result.insertId,
+            }
+            await createNotification(data, io);
         }
 
         return res.status(statusCode).json({
@@ -52,6 +64,7 @@ export const createUpdateSubscribe = asyncHandler(async (req, res) => {
 
 export const getSubscribe = asyncHandler(async (req, res) => {
     try {
+        const { filter } = req.query;
         let page = parseInt(req.query.page) || 1;
         let pageSize = parseInt(req.query.pageSize) || 10;
         let offset = (page - 1) * pageSize;
@@ -59,10 +72,13 @@ export const getSubscribe = asyncHandler(async (req, res) => {
         if (req.params?.id) {
             whereConditions.push(`id = ${req.params.id}`);
         }
+        if (filter) {
+            whereConditions.push(`id = ${filter}`);
+        }
         let whereClause = whereConditions?.length ? `WHERE ${whereConditions.join(" AND ")}` : "";
         const getSubscribeQuery = `SELECT * FROM subscribe ${whereClause} LIMIT ${pageSize} OFFSET ${offset}`;
         let countQuery = `SELECT COUNT(*) AS total FROM subscribe ${whereClause}`;
-        
+
         const getSubscribe = await db.query(getSubscribeQuery);
         const totalCountResult = await db.query(countQuery);
         const total = totalCountResult[0]?.total || 0;
